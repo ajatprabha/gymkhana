c1ae557b9bee6533f49393a97ac2f2b6536e3653
Ajat Prabha <ajat.prabha.leo@gmail.com>
2017-06-22 13:19:06 +0530

    dynamic nav-menu for society

--- main/views.py
+++ main/views.py
@@ -5,12 +5,30 @@ from .models import Society, Club
 class HomeView(TemplateView):
     template_name = 'main/index.html'
 
-
-class ClubView(DetailView):
-    template_name = 'main/base.html'
-    model = Club
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        society = Society.objects.all()
+        context['society_list'] = society
+        return context
 
 
 class SocietyView(DetailView):
-    template_name = 'main/index.html'
+    template_name = 'main/society.html'
     model = Society
+
+    def get_context_data(self, **kwargs):
+        context = super(SocietyView, self).get_context_data(**kwargs)
+        society = Society.objects.all()
+        context['society_list'] = society
+        return context
+
+
+class ClubView(DetailView):
+    template_name = 'main/club.html'
+    model = Club
+
+    def get_context_data(self, **kwargs):
+        context = super(ClubView, self).get_context_data(**kwargs)
+        society = Society.objects.all()
+        context['society_list'] = society
+        return context
